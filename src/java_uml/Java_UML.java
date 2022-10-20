03b01a063c62d04f6c4d698429fee364cd885953
Bratislav Predic <bpredic@gmail.com>
2022-10-20 23:14:20 +0200

    Init

---
+++ src/java_uml/Java_UML.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_uml;
+
+/**
+ *
+ * @author bpred
+ */
+public class Java_UML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
